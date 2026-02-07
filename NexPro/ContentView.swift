65da42a410a2a14194856ba9d1140e35c1f09a73
Jean <Tufurahi>
2026-02-07 15:00:21 -0600

    Initial Commit

---
+++ NexPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NexPro
+//
+//  Created by jean tufurahi on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NexPro/NexProApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexProApp.swift
+//  NexPro
+//
+//  Created by jean tufurahi on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct NexProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
